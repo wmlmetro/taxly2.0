309fecfd05e0c92c6fc0aad31f3023ca14a75954
Desmond Awere <desmondawere@gmail.com>
2025-10-06 03:08:35 +0100

    fix seeder issue

--- database/factories/TenantFactory.php
+++ database/factories/TenantFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Landlord;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TenantFactory extends Factory
@@ -9,6 +10,7 @@ class TenantFactory extends Factory
   public function definition(): array
   {
     return [
+      'landlord_id' => Landlord::factory(),
       'name' => $this->faker->company,
       'email' => $this->faker->unique()->safeEmail,
       'password' => bcrypt('password'), // Default password for testing
